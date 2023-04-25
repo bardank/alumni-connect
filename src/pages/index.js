d228e791963664936f3e838f55c3109da60c9076
Ajay Bhuj <bhujajay9@gmail.com>
2023-04-25 20:35:38 +0530

    footer component updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
--- src/pages/registeration.js
+++ src/pages/registeration.js
@@ -1,10 +1,26 @@
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <div>
+        <form class="flex flex-col md:flex-row md:items-center m-5 pb-3">
+          <legend className="text-xl text-black font-semibold">
+            Registeration Form
+          </legend>
+          <div>
+            <label for="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="fname"
+              placeholder="First Name"
+            />
+          </div>
+        </form>
+      </div>
       <Footer />
     </div>
   );
