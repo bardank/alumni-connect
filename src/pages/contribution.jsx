dcc66195aaf067d2d3615f33cf57187bfaf8ae7b
Ajay Bhuj <bhujajay9@gmail.com>
2023-05-26 22:41:21 +0530

    contribution page added

--- src/pages/contribution.jsx
+++ src/pages/contribution.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { Fragment } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
@@ -6,10 +7,92 @@ import PageLayout from "../layout/PageLayout";
 const Donation = () => {
   return (
     <PageLayout>
-      <h1 className="text-center font-semibold">Ways to Contribute</h1>
-      <div className="containerContribution m-4">
-        <div className="left bg-black w-72 h-72"></div>
-        <div className="right"></div>
+      <h1 className="text-center text-2xl font-bold">Ways to Contribute</h1>
+      <div className="containerContribution m-4 flex">
+        <div className="left  w-1/2 h-auto m-5 ">
+          <h2 className="font-bold underline pb-3">
+            As an alumnus/alumna, there are several ways you can contribute to
+            your college and make a positive impact:
+          </h2>
+          <ul className="list-disc">
+            <li>
+              Mentorship Programs: Offer your expertise and guidance by becoming
+              a mentor for current students. Share your experiences, provide
+              career advice, and help them navigate their academic and
+              professional journeys.
+            </li>
+            <li>
+              Guest Speaker or Panelist: Volunteer to speak at career fairs,
+              workshops, or seminars organized by your college. Share insights
+              from your industry or field of expertise, and inspire students
+              with your success story.
+            </li>
+            <li>
+              Networking Events: Participate in alumni networking events
+              organized by your college. Connect with fellow alumni, current
+              students, and faculty members. Share your professional network and
+              help others find job opportunities or internships.
+            </li>
+            <li>
+              Donations and Fundraising: Contribute financially to your
+              college's fundraising initiatives, scholarships, or specific
+              projects. Your donations can support infrastructure development,
+              research programs, or student scholarships, making education more
+              accessible for deserving students.
+            </li>
+            <li>
+              Alumni Association Involvement: Join your college's alumni
+              association and actively participate in their activities and
+              events. This involvement allows you to stay connected with your
+              alma mater and fellow alumni, fostering a sense of community and
+              providing opportunities for collaboration.
+            </li>
+            <li>
+              Alumni Testimonials: Share your success stories and experiences
+              with your college's marketing or admissions department. Your
+              testimonials can be featured on the college's website, promotional
+              materials, or social media channels, attracting prospective
+              students and enhancing the institution's reputation.
+            </li>
+          </ul>
+        </div>
+        <div class="h-auto flex items-center">
+          <div class="h-full border border-gray-500"></div>
+        </div>
+
+        <div className="right w-1/2 h-auto m-5 flex flex-col">
+          <h1 className="underline font-bold p-5 ">Bank Details</h1>
+          <table className="account-table">
+            <tr className="border-2 border-black">
+              <th className="border-2 border-black py-2 px-4">
+                Sri Krishna Institute Of Technology:
+              </th>
+              <td className="py-2 px-4">ICICI bank</td>
+            </tr>
+            <tr className="border-2 border-black">
+              <th className="border-2 border-black py-2 px-4">
+                Account Number:
+              </th>
+              <td className="py-2 px-4">1234567890</td>
+            </tr>
+            <tr className="border-2 border-black">
+              <th className="border-2 border-black py-2 px-4">Account Name:</th>
+              <td className="py-2 px-4">Your College Alumni Fund</td>
+            </tr>
+            <tr className="border-2 border-black">
+              <th className="border-2 border-black py-2 px-4">Branch:</th>
+              <td className="py-2 px-4">City Branch</td>
+            </tr>
+          </table>
+          <div className="qr-code   ">
+            <img
+              className="w-40 h-40"
+              src="./assests/icons/qrcode.png"
+              alt=""
+            />
+            <p>Scan Qr code to contribute </p>
+          </div>
+        </div>
       </div>
     </PageLayout>
   );
