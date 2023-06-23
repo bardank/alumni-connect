47f50d67672062154eb1df5f7ee9615ae5bcac48
bardan <bardankhadka@gmail.com>
2023-06-24 02:22:20 +0530

    updated modal

--- src/modals/SuccessRegistration.jsx
+++ src/modals/SuccessRegistration.jsx
@@ -3,18 +3,10 @@ import Modal from ".";
 import Button from "../components/UI/Button";
 
 const SuccessRegistration = ({}) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return (
     <Modal id="SuccessRegistration" title={"Dear Alumni"}>
       <div className="gap-8">
-        <h2 className="text-2xl font-semibold">Thank you for Registering</h2>
+        <h2 className="text-2xl font-semibold">Thank you for Registering 😊</h2>
         <p className="text-md">
           {" We'll soon verify your details and update it in our Website."}
         </p>
